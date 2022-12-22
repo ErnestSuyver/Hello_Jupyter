69e220cabee30c573bb4eeacbb95536e14954bf2
Ernest Suyver <ernestsuyver@gmail.com>
2022-12-22 09:04:39 +0100

    changed ipynb and exported it

---
+++ Hello_Jupyter.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # good afternoon
+
+# In[1]:
+
+
+print('Hello Jupyter!')
+
+
+# <p>hallootjes</p>
+
+# In[2]:
+
+
+2*2
+
+
+# In[7]:
+
+
+if 8 == 9:
+    print("wut?!"),
+else:
+    print("pindakaas")
+
+
+# groeten uit **Leiden** 
+
+# In[ ]:
+
+
+
+
